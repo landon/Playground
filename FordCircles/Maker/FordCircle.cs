a7194b5a6e880f231d3b53163df5b0b4366e6eb8
landon <landon.rabern@gmail.com>
2016-11-26 20:42:14 -0500

    colored ford circles

---
+++ FordCircles/Maker/FordCircle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maker
+{
+    class FordCircle
+    {
+        public Fraction F;
+        public double X;
+        public double Y;
+        public double R;
+
+        public FordCircle(Fraction f)
+        {
+            F = f;
+            X = (double)f.Top / f.Bottom;
+            Y = 1.0 / (2.0 * f.Bottom * f.Bottom);
+            R = 1.0 / (2.0 * f.Bottom * f.Bottom);
+        }
+    }
+}
--- FordCircles/Maker/Program.cs
+++ FordCircles/Maker/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,35 @@ namespace Maker
 {
     class Program
     {
+        const int N = 5;
+
         static void Main(string[] args)
         {
-            for (int n = 1; n < 7; n++)
+            using (var sw = new StreamWriter("Ford" + N + ".tex"))
             {
-                Console.WriteLine(string.Join(", ", new FareySequence(n)));
-            }
+                sw.WriteLine(@"\documentclass{standalone}");
+                sw.WriteLine(@"\usepackage{tikz}");
+                sw.WriteLine(@"\begin{document}");
+                sw.WriteLine(@"\begin{tikzpicture}");
+                var circles = string.Join(Environment.NewLine, new FareySequence(N).Select(f => new FordCircle(f)).Select(f => string.Format(@"\draw[black, line width=0.01pt, fill={3}] ({0},{1}) circle [radius={2}];", f.X, f.Y, f.R, Fill(f.F))));
+                sw.WriteLine(circles);
+                sw.WriteLine(@"\end{tikzpicture}");
+                sw.WriteLine(@"\end{document}");
 
-            Console.ReadKey();
+                
+            }
+        }
+
+        static string Fill(Fraction f)
+        {
+            var a = f.Top;
+            var b = f.Bottom;
+
+            if (a % 3 == b % 3)
+                return "red";
+            if (a % 3 != (b % 3) + 1)
+                return "green";
+            return "blue";
         }
     }
 }
