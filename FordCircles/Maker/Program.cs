744e0e25d46c2ee754e8cb36f02ce6f4829d4fc6
landon <landon.rabern@gmail.com>
2017-01-30 21:02:49 -0500

    tweaks

--- FordCircles/Maker/Program.cs
+++ FordCircles/Maker/Program.cs
@@ -14,11 +14,12 @@ namespace Maker
 {
     class Program
     {
-        const int N = 13;
+        const int N = 50;
 
         static void Main(string[] args)
         {
-            var vertices = new FareySequence(N).ToList();
+            var vertices = Enumerable.Range(1, N).Select(i => new Fraction() { Top = (int)f((uint)i), Bottom = (int)f((uint)i + 1) }).ToList();
+           // var vertices = new FareySequence(N).ToList();
             var adjacent = new bool[vertices.Count, vertices.Count];
             for (int i = 0; i < vertices.Count; i++)
             {
@@ -28,6 +29,9 @@ namespace Maker
                 }
             }
 
+            //var g = new Choosability.Graph(adjacent);
+            //var R = g.Vertices;
+            //var G = g.Vertices.Except(g.Vertices).ToList();
             List<int> R = null;
             List<int> G = null;
             var g = new Choosability.Graph(adjacent);
@@ -35,7 +39,7 @@ namespace Maker
             {
                 var bluegreen = g.Vertices.Except(red).ToList();
                 var h = g.InducedSubgraph(bluegreen);
-                if (!h.IsTwoColorable())
+                if (!h.IsTwoColorableSlow())
                     continue;
                 R = red;
                 foreach (var green in g.EnumerateMaximalIndependentSets(bluegreen))
@@ -51,13 +55,15 @@ namespace Maker
                     break;
             }
 
+            var B = g.Vertices.Except(R).Except(G).ToList();
+
             using (var sw = new StreamWriter("Ford" + N + ".tex"))
             {
                 sw.WriteLine(@"\documentclass{standalone}");
                 sw.WriteLine(@"\usepackage{tikz}");
                 sw.WriteLine(@"\begin{document}");
                 sw.WriteLine(@"\begin{tikzpicture}");
-                var circles = string.Join(Environment.NewLine, new FareySequence(N).Select(f => new FordCircle(f)).Select((f,i) => string.Format(@"\fill[{3}] ({0},{1}) circle [radius={2}];", f.X, f.Y, f.R, Fill(f.F, i, R, G))));
+                var circles = string.Join(Environment.NewLine, Enumerable.Range(1, N).Select(i => new Fraction() { Top = (int)f((uint)i), Bottom = (int)f((uint)i + 1) }).Select(f => new FordCircle(f)).Select((f, i) => string.Format(@"\fill[{3}] ({0},{1}) circle [radius={2}];", f.X, f.Y, f.R, Fill(f.F, i, R, G))));
                 sw.WriteLine(circles);
                 sw.WriteLine(@"\end{tikzpicture}");
                 sw.WriteLine(@"\end{document}");
@@ -69,6 +75,18 @@ namespace Maker
                 //var gg = GraphIO.GraphFromEdgeWeightString(string.Join(" ", g.GetEdgeWeights()));
                 //var s = CompactSerializer.Serialize(gg);
             }
+        }
+
+        static uint f(uint n)
+        {
+            uint a = 1, b = 0;
+            while (n != 0)
+            {
+                if ((n & 1) != 0) b += a;
+                else a += b;
+                n >>= 1;
+            }
+            return b;
         }
 
         static string Fill(Fraction f, int i, List<int> R, List<int> G)
--- HexTiling/Program.cs
+++ HexTiling/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +12,11 @@ namespace HexTiling
     {
         static void Main(string[] args)
         {
-            var colors = new[] { "black", "red", "magenta", "yellow", "cyan", "green", "gray", "blue" };
+            var colors = new[] { "black", "red!65!white", "magenta!65!white", "yellow!65!white", "cyan!65!white", "green!65!white", "gray!65!white", "blue!55!white" };
             //1,4,6,2,4,7,2,5,7,3,5
 
-            var width = 24;
+            var width = 16;
+            var height = 16;
 
             var sb = new StringBuilder();
 
@@ -22,9 +25,9 @@ namespace HexTiling
             {
                 var scope = new Scope(i * 1.5, -(i % 2) * 0.5 * Math.Sqrt(3));
                 var v = top;
-                for (int j = 0; j < 8; j++)
+                for (int j = 0; j < height; j++)
                 {
-                    scope.AddNode(colors[v], 0, -j * Math.Sqrt(3), v.ToString());
+                    scope.AddNode(colors[v], 0, -j * Math.Sqrt(3), "");
                     v++;
                     if (v > 7)
                         v -= 7;
@@ -39,6 +42,19 @@ namespace HexTiling
             }
 
             var tikz = sb.ToString();
+
+            using (var sw = new StreamWriter("plane.tex"))
+            {
+                sw.WriteLine(@"\documentclass[crop,tikz]{standalone}");
+                sw.WriteLine(@"\usepackage{tkz-graph}");
+                sw.WriteLine(@"\usetikzlibrary{shapes}
+\usetikzlibrary{shapes.geometric}");
+                sw.WriteLine(@"\begin{document}
+\begin{tikzpicture}");
+                sw.WriteLine(tikz);
+                sw.WriteLine(@"\end{tikzpicture}");
+                sw.WriteLine(@"\end{document}");
+            }
         }
     }
 }
